4c94aed8074222e954b3ad5950b660d05a22822c
MZoterov <zotsuperplus@gmail.com>
2021-06-28 20:00:20 +0300

    Initial commit for Debot

--- debots/VotingAuditsDeBot.sol
+++ debots/VotingAuditsDeBot.sol
@@ -2,3 +2,53 @@ pragma ton-solidity ^0.45.0;
 pragma AbiHeader expire;
 pragma AbiHeader pubkey;
 pragma AbiHeader time;
+
+import "./Debot.sol";
+import "./Terminal.sol";
+import "./AddressInput.sol";
+import "./Sdk.sol";
+import "./Menu.sol";
+import "./AmountInput.sol";
+import "./ConfirmInput.sol";
+import "./Transferable.sol";
+import "./Upgradable.sol";
+
+contract VotingAuditDebot is Debot {
+    bytes m_icon;
+
+    function setIcon(bytes icon) public {
+        require(msg.pubkey() == tvm.pubkey(), 100);
+        tvm.accept();
+        m_icon = icon;
+    }
+
+    /// @notice Entry point function for DeBot.
+    function start() public override {
+        Menu.select("", "Welcome to Radiance Voting Audit debot interface.", [
+            MenuItem("Get voting audits", "", 0),
+            MenuItem("Login or sign up", "", 0),
+            MenuItem("Quit", "", 0)
+        ]);
+    }
+
+    /// @notice Returns Metadata about DeBot.
+    function getDebotInfo() public functionID(0xDEB) override view returns(
+        string name, string version, string publisher, string caption, string author,
+        address support, string hello, string language, string dabi, bytes icon
+    ) {
+        name = "Radiance Voting Audit DeBot";
+        version = "0.1.0";
+        publisher = "Radiance Team";
+        caption = "DeBot for DeAudit by Radiance Team";
+        author = "Radiance Team";
+        support = address.makeAddrStd(0, 0x841288ed3b55d9cdafa806807f02a0ae0c169aa5edfe88a789a6482429756a94);
+        hello = "That's debot for Voting Audit. Developed by Radiance Team";
+        language = "en";
+        dabi = m_debotAbi.get();
+        icon = m_icon;
+    }
+
+    function getRequiredInterfaces() public view override returns (uint256[] interfaces) {
+        return [ Terminal.ID ];
+    }
+}
