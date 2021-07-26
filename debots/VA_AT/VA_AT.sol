c6c9a72e4b3f0214243916b49a040897f97be948
senitskiy <po4ta4dima@yandex.ru>
2021-07-26 14:53:13 +0300

    new root

--- debots/VA_AT/VA_AT.sol
+++ debots/VA_AT/VA_AT.sol
@@ -77,8 +77,8 @@ contract VotingAuditDebotACTMmenu is Debot {
     uint128 GRAMS_INIT_VOTE = 600000000;
     uint128 GRAMS_CREATE_DEAUDIT = 1100000000;
 
-    address DeAuditRoot = address.makeAddrStd(0, 0xaaa43758f17b6ad589924ed8dad5b7bbfcfbdbb6661135944f435b93ae542efe);
-    address m_participant = address.makeAddrStd(0, 0x4d872247fc37edb5f59dde179f3c698a6b22e58ac9defcba278180c026844a7b);
+    address DeAuditRoot = address.makeAddrStd(0, 0xeb29541ddefbe0d27642d031c2831b7f573952f3a002fb5b3a9308f7362c225c);
+    address m_participant = address.makeAddrStd(0, 0x3f9a896c0da1e0ed3efbe26a6a501020b1dd81d589f43e9f551dcdd529c5593b);
 
     bytes m_icon;
 
--- debots/VA_CL/VA_CL.sol
+++ debots/VA_CL/VA_CL.sol
@@ -73,10 +73,10 @@ contract VotingAuditDebotACTMmenu is Debot {
 //    address m_coreDebot;
 
     // address DeAuditRoot = address.makeAddrStd(0, 0xc9d81bb29dcbdf531db223524ed8e5bdc8af2073100e29d809670b542c2b5022);
-    address DeAuditRoot = address.makeAddrStd(0, 0xaaa43758f17b6ad589924ed8dad5b7bbfcfbdbb6661135944f435b93ae542efe);
+    address DeAuditRoot = address.makeAddrStd(0, 0xeb29541ddefbe0d27642d031c2831b7f573952f3a002fb5b3a9308f7362c225c);
 
     // address m_participant = address.makeAddrStd(0, 0xae3a13bc14f021a61cbcd96b80363a28c1b9fd0730a0c5fc39776852d57a62ad);
-    address m_participant = address.makeAddrStd(0, 0x4d872247fc37edb5f59dde179f3c698a6b22e58ac9defcba278180c026844a7b);
+    address m_participant = address.makeAddrStd(0, 0x3f9a896c0da1e0ed3efbe26a6a501020b1dd81d589f43e9f551dcdd529c5593b);
 
     bytes m_icon;
 
